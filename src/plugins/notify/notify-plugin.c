128ee006cc2c9d70ae7464bbc962564aaabd6b44
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-03-31 16:10:31 +0700

    debug push-notification mailbox event

--- src/plugins/notify/notify-plugin.c
+++ src/plugins/notify/notify-plugin.c
@@ -151,12 +151,12 @@ void notify_contexts_mail_transaction_rollback(struct mailbox_transaction_contex
 void notify_contexts_mailbox_create(struct mailbox *box) 
 {
 	struct notify_context *ctx;
+	i_debug("Notify - notify_contexts_mailbox_create: %s", &box->name);
 
 	for (ctx = ctx_list; ctx != NULL; ctx = ctx->next) {
 		if (ctx->v.mailbox_create != NULL)
 			ctx->v.mailbox_create(box);
 	}
-	i_debug("Notify - notify_contexts_mailbox_create: %s", &box->name);
 }
 
 void notify_contexts_mailbox_update(struct mailbox *box)

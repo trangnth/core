e2aaabce0603448c305c757a1197c276ae51e727
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-05-08 09:24:56 +0700

    debug event flags set

--- src/plugins/push-notification/push-notification-txn-msg.c
+++ src/plugins/push-notification/push-notification-txn-msg.c
@@ -35,15 +35,16 @@ push_notification_txn_msg_create(struct push_notification_txn *txn,
     i_debug("EEEE - uid = %d, seq = %d", msg->uid, msg->seq);
 
     struct push_notification_txn_msg_uid *uid, *u;
+    uid = p_new(txn->pool, struct push_notification_txn_msg_uid, 1);
     uid->uid = msg->uid;
     uid->seq = msg->seq;
     array_append(&msg->uids, &uid, 1);
     i_debug("EEEE -> uid->uid: %d", uid->uid);
     
-    // // uint32_t u;
-    // array_foreach(&msg->uids, u){
-    //     i_debug ("UUU 22:  %d", u->uid);
-    // }
+    // uint32_t u;
+    array_foreach(&msg->uids, u){
+        i_debug ("UUU 22:  %d", u->uid);
+    }
 
     hash_table_insert(txn->messages, POINTER_CAST(txn->t->save_count + 1),
                         msg);

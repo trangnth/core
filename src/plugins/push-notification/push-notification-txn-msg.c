71f94b456f3361cb0ce86a16a255d23efd144a15
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-05-08 09:07:43 +0700

    debug event flags set

--- src/plugins/push-notification/push-notification-txn-msg.c
+++ src/plugins/push-notification/push-notification-txn-msg.c
@@ -33,11 +33,12 @@ push_notification_txn_msg_create(struct push_notification_txn *txn,
     msg->seq = txn->t->save_count;
     msg->uid = mail->uid;
     i_debug("EEEE - uid = %d, seq = %d", msg->uid, msg->seq);
-    
+
     struct push_notification_txn_msg_uid *uid, *u;
     uid->uid = msg->uid;
-    uid->seq = msg->seq;
-    array_append(&msg->uids, &uid, 1);
+    // uid->seq = msg->seq;
+    // array_append(&msg->uids, &uid, 1);
+    i_debug("EEEE -> uid->uid: %d", uid->uid);
     
     // // uint32_t u;
     // array_foreach(&msg->uids, u){

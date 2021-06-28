ac545cdca44d1a867e9307ebd21746c4329da081
trangnth <trangnguyenthihuyen@vccorp.vn>
2021-06-28 14:52:56 +0700

    add uidvalidity to message in mailboxdelete event

--- src/plugins/push-notification/push-notification-txn-mbox.c
+++ src/plugins/push-notification/push-notification-txn-mbox.c
@@ -41,7 +41,7 @@ push_notification_txn_mbox_end(struct push_notification_txn *ptxn,
         // hash_table_iterate(hiter, ptxn->messages, &key, &value);
         // if (changes->uid_validity == 0) {
         mailbox_get_open_status(ptxn->mbox, STATUS_UIDVALIDITY, &status);
-        value->uid_validity = status.uidvalidity;
+        // value->uid_validity = status.uidvalidity;
         i_debug ("push_notification_txn_mbox_end - MSG234234 - uidvalidity %u", status.uidvalidity);
         // } else {
         //     value->uid_validity = changes->uid_validity;

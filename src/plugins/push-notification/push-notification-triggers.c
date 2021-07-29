e28695602f7a1315ebff4f3ee35c08f53b740e6e
trangnth <nguyenhuyentrang1996@gmail.com>
2021-07-29 22:53:51 +0700

    add message-id to message messageexpunge event

--- src/plugins/push-notification/push-notification-triggers.c
+++ src/plugins/push-notification/push-notification-triggers.c
@@ -116,7 +116,7 @@ push_notification_trigger_msg_common(struct push_notification_txn *txn,
                                      enum push_notification_event_trigger trigger)
 {
     i_debug ("DDDDDD - push_notification_trigger_msg_common");
-    const char *value;
+    
 
     
     if (*msg == NULL) {
@@ -124,10 +124,10 @@ push_notification_trigger_msg_common(struct push_notification_txn *txn,
         *msg = push_notification_txn_msg_create(txn, mail);
         i_debug ("1");
     }
-
-    i_debug ("2");
-    mail_get_first_header(mail, "Message-ID", &value);
-    i_debug ("TRAAAAAAA - data->msgid: %s", mail_get_first_header(mail, "Message-ID", &value));
+#    const char *value;
+    // i_debug ("2");
+    // mail_get_first_header(mail, "Message-ID", &value);
+    // i_debug ("TRAAAAAAA - data->msgid: %s", mail_get_first_header(mail, "Message-ID", &value));
 
     txn->trigger |= trigger;
 }
